3fbb5a627be7c92c9f1d0cd33c0acf1219c37738
Steve Leung <stevealeung@gmail.com>
2020-11-16 18:36:45 -0800

    modified circle_animation.py to add an outline drawing

--- circle_animation.py
+++ circle_animation.py
@@ -2,6 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
+# setup
+cmap = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 # get the coordinates for a circle
 radiusCircle = float(1)
@@ -16,8 +18,9 @@ yLocsCircle = np.concatenate((y1,y2))
 fig = plt.figure()
 ax = plt.axes(xlim=(-2, 2), ylim=(-2, 2))
 ax.set_aspect('equal', 'box')
-circle, = ax.plot(xLocsCircle, yLocsCircle, linewidth=0.5, color=[0,0,0])
-line, = ax.plot([], [], linewidth=2)
+circle, = ax.plot(xLocsCircle, yLocsCircle, linewidth=0.5, color=cmap[0])
+line, = ax.plot([], [], linewidth=2, color=cmap[0])
+outline, = ax.plot([], [], linewidth=2, color=[0,0,0])
 
 numFrames = 200
 
@@ -28,14 +31,27 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(i):
-    r = float(1)
-    angle = float(i)/numFrames*2*np.pi
-    xPos = np.cos(angle)/r
-    yPos = np.sin(angle)/r
-    x = np.array([0, xPos])
-    y = np.array([0, yPos])
-    line.set_data(x, y)
-    return line,
+    radiusCircle = float(1)
+    angle_rad = float(i)/numFrames*2*np.pi
+    xPos = np.cos(angle_rad)/radiusCircle
+    yPos = np.sin(angle_rad)/radiusCircle
+    # line drawing
+    xLine = np.array([0, xPos])
+    yLine = np.array([0, yPos])
+    line.set_data(xLine, yLine)
+    # outline drawing
+    if i < numFrames/2:
+        xOutline = np.linspace(1, xPos, 100)
+        yOutline = np.sqrt(np.power(radiusCircle,2)-np.power(xOutline,2))
+    else:
+        xOutline1 = np.linspace(1, -1, 100)
+        xOutline2 = np.linspace(-1, xPos, 100)
+        yOutline1 = np.sqrt(np.power(radiusCircle,2)-np.power(xOutline1,2))
+        yOutline2 = -np.sqrt(np.power(radiusCircle,2)-np.power(xOutline2,2))
+        xOutline = np.concatenate((xOutline1,xOutline2))
+        yOutline = np.concatenate((yOutline1,yOutline2))
+    outline.set_data(xOutline, yOutline)
+    return line, outline,
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
